5885124b3ebfb10a0c87f32cc7a26154e4a4a820
Insilvon <colincrowe14@gmail.com>
2018-12-31 12:53:00 -0500

    First Commit - Core Plugin Setup

---
+++ src/main/java/me/insilvonplugins/movecraft/Movecraft.java
@@ -0,0 +1,17 @@
+package me.insilvonplugins.movecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Movecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
